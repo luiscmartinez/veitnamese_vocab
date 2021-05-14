002232aff770cf345302b8ea3df183164ca3a83e
Luis Martinez <mrluismartinezzz@gmail.com>
2021-05-13 20:07:07 -0700

    init obj of classifiers

---
+++ classifiers.js
@@ -0,0 +1,10 @@
+const classifiers = {
+  con: "indicate animacy",
+  cái: "indicate inanimacy",
+  cây: "long and slender",
+  cuốn: "long and cylindrical",
+  mảnh: "small piece",
+  bộ: "set of",
+  nhóm: "group of",
+  đàn: "flock of, herd of",
+};
